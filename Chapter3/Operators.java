fda992d0f13a1e53a80a30608915333c5859693d
Sonu Prajapati <sonukn089@gmail.com>
2017-08-24 23:28:41 +0530

    Chapter3

---
+++ Chapter3/Operators.java
@@ -0,0 +1,25 @@
+class Operators{
+	
+         public static void main(String[] args) {
+      	
+           int x = 1;
+
+           boolean a = (x > 0);
+
+           boolean b = (x < 0);
+
+           boolean c = (x != 0);
+
+           boolean d = (x >= 0);
+
+           boolean e = (x != 1);
+            
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(c);
+            System.out.println(d);
+            System.out.println(e);
+
+           }
+
+}
