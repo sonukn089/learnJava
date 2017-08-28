32de437cf6e2a4db7b192afe55156850829386b8
Sonu Prajapati <sonukn089@gmail.com>
2017-08-29 00:51:53 +0530

    Chapter3

---
+++ Chapter3/Suppose.java
@@ -0,0 +1,31 @@
+class Suppose{
+	
+        public static void main(String[] args) {
+      	
+          int x = 3, y = 2, z;
+
+      	  if (x > 2){
+
+      	  	 if (y > 2) {
+
+      	  	 	z = x + y;
+      	  	 	
+              System.out.print("Z is : " + z);
+
+      	  	  }
+
+      	  	  else
+      	  	  	System.out.print("Nothing");
+      	  }
+
+
+      	  else{
+      	  	 System.out.print("X is : " + x);
+      	  }
+
+
+    }
+
+
+
+}
