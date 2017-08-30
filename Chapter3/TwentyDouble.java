2894ff994767e22442d9bb279de131b77cba0a35
Sonu Prajapati <sonukn089@gmail.com>
2017-08-30 23:57:38 +0530

    Chapter3

---
+++ Chapter3/TwentyDouble.java
@@ -0,0 +1,24 @@
+class RandomDouble{
+	
+	      public static void main(String[] args) {
+      	
+			       double a = (double)(Math.random() * 20);  //(int)(Math.random() * 20)
+    	
+			       System.out.println(a);
+
+			       double b = 10 + (double)(Math.random() * 10); //10 + (int)(Math.random() * 10)
+
+			       System.out.println(b);
+
+			       double c = 10 + (double)(Math.random() * 41);  //10 + (int)(Math.random() * 41) 
+
+			       System.out.println(c);
+
+			       double d = (double)(Math.random() * 2);   //(int)(Math.random() * 2)
+			       
+			       System.out.println(d);
+  			       
+      }
+
+
+}
