40e148cf8bb753edef0c93e45c84b75c8454b985
Sonu Prajapati <sonukn089@gmail.com>
2017-08-30 23:48:06 +0530

    Chapter3

---
+++ Chapter3/RandomTwenty.java
@@ -0,0 +1,24 @@
+class RandomTwenty{
+	
+	      public static void main(String[] args) {
+      	
+			       int a = (int)(Math.random() * 10);  //(int)(Math.random() * 20)
+    	
+			       System.out.println(a);
+
+			       int b = 10 + (int)(Math.random() * 10); //10 + (int)(Math.random() * 10)
+
+			       System.out.println(b);
+
+			       int c = 10 + (int)(Math.random() * 41);  //10 + (int)(Math.random() * 41) 
+
+			       System.out.println(c);
+
+			       int d = (int)(Math.random() * 2);   //(int)(Math.random() * 2)
+			       
+			       System.out.println(d);
+  			       
+      }
+
+
+}
