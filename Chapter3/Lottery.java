751d5182e00b48a2df2eb3b9be98fa24e60c2c32
Sonu Prajapati <sonukn089@gmail.com>
2017-09-02 14:39:30 +0530

    Chapter3

---
+++ Chapter3/Lottery.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+class Lottery{
+	
+       public static void main(String[] args) {
+    	
+	    	Scanner input = new Scanner(System.in);
+
+	    	int lottery = (int)(Math.random() * 100);
+
+	    	int lotteryDigit1 = lottery / 10;
+
+	    	int lotteryDigit2 = lottery % 10;
+
+	    	System.out.print("Enter your lottery pick ( two digits ) : ");
+
+	    	int guess = input.nextInt();
+
+            int guessDigit1 = guess / 10;
+
+            int guessDigit2 = guess % 10;
+
+            System.out.println("The lottery number is "  +   lottery);
+
+            if (guess == lottery) {
+
+            	System.out.println("Exact match : You win $10,000");
+            	
+            }
+
+            else if (guessDigit2 == lotteryDigit1 && guessDigit1 == guessDigit2)  {
+
+            	System.out.println("Match all digits : you win $3,000");
+            	
+            }
+            
+            else if (guessDigit1 == lotteryDigit1
+            	 ||  guessDigit1 == lotteryDigit2
+                 ||  guessDigit2 == lotteryDigit1    	 
+                 ||  guessDigit2 == lotteryDigit2 ){
+
+            	System.out.print("Match one digit : you win $1,000");
+            	
+            }
+
+            else
+            	System.out.print(" Sorry, no match");
+
+
+
+
+         }
+
+}
