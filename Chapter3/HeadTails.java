ee71265ba337c949ba5a67131de993eee7d884b2
Sonu Prajapati <sonukn089@gmail.com>
2017-09-15 23:52:30 +0530

    Head Or Tails

---
+++ Chapter3/HeadTails.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class HeadTails{
+	
+		   public static void main(String[] args) {
+
+				   	 Scanner input = new Scanner(System.in);
+
+				   	 int x = (int)(Math.random() * 2);
+
+				   	 if (x == 0) {
+				   	 	System.out.print("Head");
+				   	 	
+				   	 }
+		             else{
+
+		             	System.out.print("Tails");
+		             }
+
+
+
+		   	
+		     }
+
+
+}
