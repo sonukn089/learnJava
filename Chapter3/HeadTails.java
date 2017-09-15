989bd35defcc44e13710570504f6b5cc5793166c
Sonu Prajapati <sonukn089@gmail.com>
2017-09-16 01:04:06 +0530

    Head or Tail

--- Chapter3/HeadTails.java
+++ Chapter3/HeadTails.java
@@ -2,25 +2,24 @@ import java.util.Scanner;
 
 class HeadTails{
 	
-		   public static void main(String[] args) {
+	public static void main(String[] args) {
 
-				   	 Scanner input = new Scanner(System.in);
-
-				   	 int x = (int)(Math.random() * 2);
-
-				   	 if (x == 0) {
-				   	 	System.out.print("Head");
-				   	 	
-				   	 }
-		             else{
-
-		             	System.out.print("Tails");
-		             }
+		Scanner input = new Scanner(System.in);
 
 
+		System.out.print("Please guess Head (0) or Tails (1) : ");
+		String a = input.next();
 
+		int x = (int)(Math.random() * 2);
+
+		// Genrate 0 or 1
+		if (x == 0)
+			System.out.print("Head");
+		else
+			System.out.print("Tails");
 		   	
-		     }
-
+	}
 
 }
+
+
