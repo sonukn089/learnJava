45138e913018b574df3071d09459d236462259dc
Sonu Prajapati <sonukn089@gmail.com>
2017-09-17 01:24:35 +0530

    Cost of Shipping

--- Chapter3/CostOfShipping.java
+++ Chapter3/CostOfShipping.java
@@ -10,28 +10,44 @@ class CostOfShipping{
 
 	 	double weight = input.nextDouble();
 
-	 	if (weight <= 1 ) {
+	 	double cost = 0;
 
-	 		System.out.print("your cost is " + (weight * 3.15) );	 		
+	 	if (weight > 0 && weight <= 1 ) {
+
+	 		System.out.print("your shipping cost is " + (weight * 3.15) );	 		
 	 	}
-	 	else if (weight <= 3) {
+	 	else if (weight > 1 && weight <= 3) {
 
-	 		System.out.print("your cost is " + (weight * 5.5));
+	 		System.out.print("your shipping cost is " + (weight * 5.5));
 	 		
 	 	}
-	 	else if (weight <= 10) {
+	 	else if (weight > 3 && weight <= 10) {
 
-	 		System.out.print("your cost is " + (weight * 8.5));
+	 		System.out.print("your shipping cost is " + (weight * 8.5));
 	 		
 	 	}
 
-	 	else if (weight <= 50) {
+	 	else if (weight > 10 && weight <= 20) {
 
-	 		System.out.print("your cost is " + (weight * 10.5));
+	 		System.out.print("your shipping cost is " + (weight * 10.5));
+
 	 		
 	 	}
+	 	else if (weight > 20 && weight <= 30) {
+
+	 		System.out.print("your shipping cost is " + (weight * 10.5));
+	 	}
+	 	else if (weight > 30 && weight <= 40) {
+
+	 		System.out.print("your shipping cost is " + (weight * 10.5));
+	 	}
+	 	else if (weight > 40 && weight <= 50) {
+
+	 		System.out.print("your shipping cost is " + (weight * 10.5));
+	 	}
+
          else
-         	System.out.print("your weight is grater than 50, The package cannot be shipped");
+         	System.out.print("your package weight is grater than 50, The package cannot be shipped");
 
 	}
 
