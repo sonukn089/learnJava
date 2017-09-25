9ea6233fad8b9ffc84c5bcb33f4ec17aa8ab4679
Sonu Prajapati <sonukn089@gmail.com>
2017-09-26 00:48:01 +0530

    Check

---
+++ Chapter5/Check.java
@@ -0,0 +1,21 @@
+class Check{
+	
+	public static void main(String[] args) {
+		
+		  // int i = 1;
+
+    //       while (i < 10)
+
+    //       if ((i++) % 2 == 0)
+
+    //       System.out.println(i);
+             int x = 80000000;
+
+while (x > 0) 
+  x++;
+
+System.out.println("x is " + x);
+
+
+	}
+}
