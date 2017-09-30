67433f50c7e235d5df0c02956f414a93956df054
Sonu Prajapati <sonukn089@gmail.com>
2017-09-30 19:38:11 +0530

    Ascii Value & Char

---
+++ Chapter5/ScaiiValue.java
@@ -0,0 +1,17 @@
+class ScaiiValue{
+	
+	public static void main(String[] args) {
+
+		char ch;
+		
+		int i = 1;
+
+		for (i = 1; i <= 255; i++) {
+
+			 ch = (char)i;
+
+			 System.out.println(i + " = " + ch);
+			
+		}
+	}
+}
