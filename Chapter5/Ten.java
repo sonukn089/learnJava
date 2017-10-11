6ed699821ef9122024af9e87825e872a4aa183f6
Sonu Prajapati <sonukn089@gmail.com>
2017-10-12 00:46:02 +0530

    Name

--- Chapter5/Ten.java
+++ Chapter5/Ten.java
@@ -7,12 +7,17 @@ class Ten{
 			for (int j = 1; j<= 19; j++) {
 
 				if (i==1 || i==5 || j==1 || j==19)
+
 					System.out.print(" *");
+
 				else if(i == 3){
+
 					if(j == 3)
+
 						System.out.print("   M y   N a m e   -I s   S o n u  ");
 				}
 				else
+					
 					System.out.print("  ");
 			 }
 
