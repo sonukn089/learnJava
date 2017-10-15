2c40433cde9c4d84ecb2c7d0b0132c5083f39049
Sonu Prajapati <sonukn089@gmail.com>
2017-10-16 00:10:08 +0530

    All

---
+++ TwentyOne/LeapYears.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class LeapYears{
+	
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+        
+		System.out.print("Please input the year = ");
+		int y = input.nextInt();
+
+		if (y % 4 == 0) {
+
+			System.out.print(y + " is Leap-Year ");
+			
+		}
+
+
+		else
+			System.out.print(y + " is not Leap-Year ");
+
+  }
+
+}
