53161a585034b68a062f6e574daaca8912e60958
Sonu Prajapati <sonukn089@gmail.com>
2017-10-24 23:15:34 +0530

    Character

---
+++ Chapter5/Character.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class Character{
+	
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+		char ch;
+        Scanner scan = new Scanner(System.in);
+		
+        System.out.print("Please input any Character : ");
+        ch = scan.next().charAt(0);
+		
+        if((ch >='a' && ch <='z') || (ch >='A' && ch <='Z'))
+        {
+            System.out.print(ch + " is an Alphabet");
+        }
+        else if ((ch >= 48 && ch <= 57)) {
+        
+            System.out.print(ch + " is an Number");
+            	
+        }
+        else{
+
+            System.out.print(ch + " is an Special Character");
+        }
+		
+	}
+}
