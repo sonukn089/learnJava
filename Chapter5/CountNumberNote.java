85d1111c075edc7aefe714894f23c35bcd131eaa
Sonu Prajapati <sonukn089@gmail.com>
2017-10-26 00:35:29 +0530

    Note Count

--- Chapter5/CountNumberNote.java
+++ Chapter5/CountNumberNote.java
@@ -5,19 +5,82 @@ class CountNumberNote{
       public static void main(String[] args) {
 
 		      Scanner input = new Scanner(System.in);
-          int a = 0;
+        
+         int a;
           
           System.out.print("Please input the number : ");
 
           int n = input.nextInt();
+
+            a = n / 2000;
+            n = n % 2000;
+            System.out.println(a + " 2000 ke Note ");
+            
+
+            a = n / 500;
+            n = n % 500;
+
+           System.out.println(a + " 500 ke Note ");
+
+            a = n / 100;
+            n = n % 100;
+
+
+           System.out.println(a + " 100 ke Note"); 
+
+            a = n / 50;
+            n = n % 50;
+           System.out.println(a + " 50 ke Note");
+  
+            a = n / 20;
+            n = n % 20;
+
+            System.out.println(a + " 20 ke Note");
+
+            a = n / 10;
+            n = n % 10;
+
+            System.out.println(a + " 10 ke Note");
+
+            a = n / 5;
+            n = n % 5;
+
+            System.out.println(a + " 5 ke Note");
+            
+            a = n / 2;
+            n = n % 2;
+
+            System.out.println(a + " 2 ke Note");
+
+            a = n / 1;
+
+            System.out.print(a + " 1 ke Note");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
           
 
-          if(n % 2000 == 0){
-            while(n!=0)
-            a++;
 
-            System.out.print(a);
-          }
+            
+            
+          
 
 
 
