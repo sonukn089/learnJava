7734b36e5162df4fd11727d6135f11a04bff42e1
Sonu Prajapati <sonukn089@gmail.com>
2017-10-25 22:36:11 +0530

    Count Note

---
+++ Chapter5/CountNote.java
@@ -0,0 +1,134 @@
+import java.util.Scanner;
+
+class CountNote{
+	
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+	    
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        int d = 0;
+        int e = 0;
+        int f = 0;
+        int g = 0;
+        int h = 0;
+        int i = 0;	
+
+        System.out.print("Please input the Number : ");
+        int n = input.nextInt();
+        
+while(n>0)
+if (n%2000 == 0 || n%500 == 0 || n%100 == 0 || n%50 == 0 || n%20 == 0 || n%10 == 0 || n%5 == 0 || n%2 == 0 || n%1 == 0) {
+          a++;
+          b++;
+          c++;
+          d++;
+          e++;
+          f++;
+          g++;
+          h++;
+          i++;
+
+          System.out.print(a +" 2000 "+b+" 500 "+c+" 100 "+d+" 50 "+e+" 20 "+f+" 10 "+g+" 5 "+h+" 2 "+i+" 1 ");  
+
+        }
+
+else if (n%500 == 0 || n%100 == 0 || n%50 == 0 || n%20 == 0 || n%10 == 0 || n%5 == 0 || n%2 == 0 || n%1 == 0) {
+          b++;
+          c++;
+          d++;
+          e++;
+          f++;
+          g++;
+          h++;
+          i++;
+
+          System.out.print(b+" 500 "+c+" 100 "+d+" 50 "+e+" 20 "+f+" 10 "+g+" 5 "+h+" 2 "+i+" 1 ");  
+
+        }
+
+
+else if (n%100 == 0 || n%50 == 0 || n%20 == 0 || n%10 == 0 || n%5 == 0 || n%2 == 0 || n%1 == 0) {
+          c++;
+          d++;
+          e++;
+          f++;
+          g++;
+          h++;
+          i++;
+
+          System.out.print(c+" 100 "+d+" 50 "+e+" 20 "+f+" 10 "+g+" 5 "+h+" 2 "+i+" 1 ");  
+
+        }
+
+
+else if (n%50 == 0 || n%20 == 0 || n%10 == 0 || n%5 == 0 || n%2 == 0 || n%1 == 0) {
+          d++;
+          e++;
+          f++;
+          g++;
+          h++;
+          i++;
+
+          System.out.print(d+" 50 "+e+" 20 "+f+" 10 "+g+" 5 "+h+" 2 "+i+" 1 ");  
+
+        }        
+
+
+else if (n%20 == 0 || n%10 == 0 || n%5 == 0 || n%2 == 0 || n%1 == 0) {
+          e++;
+          f++;
+          g++;
+          h++;
+          i++;
+
+          System.out.print(e+" 20 "+f+" 10 "+g+" 5 "+h+" 2 "+i+" 1 ");  
+
+        }        
+
+
+else if (n%10 == 0 || n%5 == 0 || n%2 == 0 || n%1 == 0) {
+          f++;
+          g++;
+          h++;
+          i++;
+
+          System.out.print(f+" 10 "+g+" 5 "+h+" 2 "+i+" 1 ");  
+
+        }        
+
+else if (n%5 == 0 || n%2 == 0 || n%1 == 0) {
+          g++;
+          h++;
+          i++;
+
+          System.out.print(g+" 5 "+h+" 2 "+i+" 1 ");  
+
+        }        
+
+
+else if (n%2 == 0 || n%1 == 0) {
+          h++;
+          i++;
+
+          System.out.print(h+" 2 "+i+" 1 ");  
+
+        }        
+
+
+else if (n%1 == 0) {
+          i++;
+
+          System.out.print(i+" 1 ");  
+
+        }        
+
+
+
+      
+		
+	}
+        
+}
