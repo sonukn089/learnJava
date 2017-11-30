6434e230af2d4a3e3ab1110a74850d2c3c81f049
Sonu Prajapati <sonukn089@gmail.com>
2017-11-30 23:13:52 +0530

    Guess Number In loop

---
+++ Chapter5/True.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+class True{
+	
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+		int number = (int)(Math.random() * 101);
+
+		System.out.println("Guess a magic number between 0 and 100");
+
+		int guess = -1;
+
+		while (guess != number){
+
+			System.out.print("\n Enter your guess: ");
+
+			guess = input.nextInt();
+
+			if (guess == number) 
+
+				System.out.println("Yes, the number is " + number);
+				
+
+			else if (guess > number) 
+
+				System.out.println("your gue is too high");						
+					
+			else
+			System.out.println("Your guess is too low");	
+			
+		}
+
+
+	}
+}
