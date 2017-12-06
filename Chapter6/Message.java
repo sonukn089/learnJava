647bf61a8b82f4fc22821fe35f8101b8139d14f1
Sonu Prajapati <sonukn089@gmail.com>
2017-12-07 00:29:14 +0530

    Message

---
+++ Chapter6/Message.java
@@ -0,0 +1,15 @@
+class Message{
+	
+	public static void main(String[] args) {
+		
+		nPrintln("sonu", 10);
+
+	}
+	public static void nPrintln(String message, int n){
+
+		for (int i = 0; i < n; i++) {
+			System.out.println(message);
+			
+		}
+	}
+}
