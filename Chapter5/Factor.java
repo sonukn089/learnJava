f50a9de736f58b7dd2c3ec21508c68e26fababae
Sonu <sonukn089@gmail.com>
2018-02-14 22:35:10 +0530

    Factor Programm

---
+++ Chapter5/Factor.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class Factor{
+	
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Pleas input the number: ");
+		int number = input.nextInt();
+
+		int fact = 2;
+
+		while (number != 1){
+
+			if (number % fact == 0) {
+
+				System.out.print(fact + " ");
+
+				number /= fact;
+				
+			}
+			else
+				++fact;
+		}
+	}
+}
