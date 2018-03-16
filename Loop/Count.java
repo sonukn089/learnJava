a5e16ce9f229a8252f9832ba0b2642b9d06b2c4c
Sonu <sonukn089@gmail.com>
2018-03-16 23:58:29 +0530

    loop

---
+++ Loop/Count.java
@@ -0,0 +1,16 @@
+class Count{
+	
+	public static void main(String[] args) {
+		
+		int count = 0;
+
+		while (count < 100){
+
+			System.out.println("Welcome to Java !");
+
+			count++;
+
+		}
+		System.out.println("--------------------");
+	}
+}
